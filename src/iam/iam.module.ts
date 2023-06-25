a39299c36e70c7b0f513280f042fcb64083e5562
Gabriel <seufernandezdev@gmail.com>
2023-06-25 02:33:33 -0300

    add guards to entire application

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -5,16 +5,30 @@ import { AuthenticationController } from './authentication/authentication.contro
 import { AuthenticationService } from './authentication/authentication.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from './config/jwt.config';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig), // so JWT can be injected by the authentication service
+  ],
   providers: [
     {
       provide: HashingService,
       useClass: BcryptService,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard, // enabling guards to entire application to all routes
     },
     AuthenticationService,
   ],
   controllers: [AuthenticationController],
 })
 export class IamModule {}
+// ('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImVtYWlsIjoiZ2FicmllbEBnYWJyaWVsLmNvbSIsImlhdCI6MTY4NzY3MDkwNywiZXhwIjoxNjg3Njc0NTA3LCJhdWQiOiJsb2NhbGhvc3Q6MzAwMCIsImlzcyI6ImxvY2FsaG9zdDozMDAwIn0.hp9XDrmW7zRuOGqwbZtnGYZDHGNluR-jtwHPakm_T5g');
