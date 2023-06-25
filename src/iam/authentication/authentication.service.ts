3e4659bef355dac44648fde4412fbd7acba74dca
Gabriel <seufernandezdev@gmail.com>
2023-06-24 23:50:15 -0300

    add authentication

---
+++ src/iam/authentication/authentication.service.ts
@@ -0,0 +1,57 @@
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '../hashing/hashing.service';
+import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.password = await this.hashingService.hash(signUpDto.password);
+
+      await this.usersRepository.save(user);
+    } catch (error) {
+      const pgUniqueViolationErrorCode = '23505';
+      if (error.code === pgUniqueViolationErrorCode) {
+        throw new ConflictException();
+      }
+      throw error;
+    }
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const user = await this.usersRepository.findOneBy({
+      email: signInDto.email,
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid Credentials');
+    }
+
+    const isEqual = await this.hashingService.compare(
+      signInDto.password,
+      user.password,
+    );
+
+    if (!isEqual) {
+      throw new UnauthorizedException('Invalid Credentials');
+    }
+
+    //filling this gap soon
+    return true;
+  }
+}
